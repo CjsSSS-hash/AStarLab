c83dd2f198948e8c4d711144d50c77754e4f916e
XKIrito <calloracj90@gmail.com>
2025-08-14 21:28:31 +0800

    Node Class

---
+++ Main.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+
+ class Node {
+    int x, y; // Coordinates
+    double g, h, f; // Costs
+    Node parent; // Parent node for path tracing
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.g = 0;
+        this.h = 0;
+        this.f = 0;
+        this.parent = null;
+    }
+
+}
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+        
+    }
+    
+}
